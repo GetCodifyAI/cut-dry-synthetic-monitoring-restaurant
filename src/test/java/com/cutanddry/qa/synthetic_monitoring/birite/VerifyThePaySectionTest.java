d2f21c7b1a0dc1901e7f3b6f36e533a73fefa315
senuri-mr <senuri@cutanddry.com>
2024-10-30 12:05:53 +0530

    Added new tests

---
+++ src/test/java/com/cutanddry/qa/synthetic_monitoring/birite/VerifyThePaySectionTest.java
@@ -0,0 +1,45 @@
+package com.cutanddry.qa.synthetic_monitoring.birite;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Pay;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyThePaySectionTest extends TestBase {
+    static User user;
+    static String DP = "30785489";
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test
+    public void VerifyThePaySection() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.logIntoRestaurantProd(user.getEmailOrMobile(), user.getPassword());
+        softAssert.assertTrue(Dashboard.isUserNavigatedToRestaurantDashboard(),"login error");
+        Login.navigateToLoginAsPortal(DP);
+        Dashboard.navigateToPay();
+        softAssert.assertTrue(Pay.isUserNavigatedToPay(),"navigation error");
+        Pay.clickOnOutstanding();
+        softAssert.assertTrue(Pay.isOutstandingBtnSelected(),"outstanding btn select error");
+        Pay.clickOnPaid();
+        softAssert.assertTrue(Pay.isPaidBtnSelected(),"paid btn select error");
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+}
