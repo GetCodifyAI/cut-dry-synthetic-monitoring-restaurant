af629921af830404b886af5617071048aaf8947c
senuri-mr <senuri@cutanddry.com>
2024-11-04 13:36:30 +0530

    Modified

---
+++ src/test/java/com/cutanddry/qa/synthetic_monitoring/wcw/owner/VerifyTheDraftsTest.java
@@ -0,0 +1,41 @@
+package com.cutanddry.qa.synthetic_monitoring.wcw.owner;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Draft;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyTheDraftsTest extends TestBase {
+    static User user;
+    static String DP = "211953812";
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test
+    public void VerifyTheDrafts() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.logIntoRestaurantProd(user.getEmailOrMobile(), user.getPassword());
+        softAssert.assertTrue(Dashboard.isUserNavigatedToRestaurantDashboard(),"login error");
+        Login.navigateToLoginAsPortal(DP);
+        Dashboard.navigateToDrafts();
+        softAssert.assertTrue(Draft.isUserNavigatedToDrafts(),"navigation error");
+        softAssert.assertAll();
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+}
