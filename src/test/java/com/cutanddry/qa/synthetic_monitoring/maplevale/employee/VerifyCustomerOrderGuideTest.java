8607ed7a5dfa70ac8a992ca8a39022669a4fb64b
senuri-mr <senuri@cutanddry.com>
2024-11-06 11:19:21 +0530

    Modified

---
+++ src/test/java/com/cutanddry/qa/synthetic_monitoring/maplevale/employee/VerifyCustomerOrderGuideTest.java
@@ -0,0 +1,42 @@
+package com.cutanddry.qa.synthetic_monitoring.maplevale.employee;
+
+import com.cutanddry.qa.base.TestBase;
+import com.cutanddry.qa.data.models.User;
+import com.cutanddry.qa.functions.Dashboard;
+import com.cutanddry.qa.functions.Login;
+import com.cutanddry.qa.functions.Orders;
+import com.cutanddry.qa.utils.JsonUtil;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class VerifyCustomerOrderGuideTest extends TestBase {
+    static User user;
+    static String DP = "daniel.goerndt@scout";
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        user = JsonUtil.readUserLogin();
+    }
+
+    @Test
+    public void VerifyCustomerOrderGuide() throws InterruptedException {
+        SoftAssert softAssert = new SoftAssert();
+        Login.logIntoRestaurantProd(user.getEmailOrMobile(), user.getPassword());
+        softAssert.assertTrue(Dashboard.isUserNavigatedToRestaurantDashboard(),"login error");
+        Login.navigateToLoginAsPortal(DP);
+        Dashboard.navigateToOrders();
+        softAssert.assertTrue(Orders.isUserNavigatedToOrderGuide(),"navigation error");
+        softAssert.assertAll();
+
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        takeScreenshotOnFailure(result);
+        closeAllBrowsers();
+    }
+}
