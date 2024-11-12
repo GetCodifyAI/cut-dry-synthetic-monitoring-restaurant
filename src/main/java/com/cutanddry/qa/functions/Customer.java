47eb8600d852328be67741c8161a7bf56303e44b
senuri-mr <senuri@cutanddry.com>
2024-11-12 17:55:52 +0530

    Modified

--- src/main/java/com/cutanddry/qa/functions/Customer.java
+++ src/main/java/com/cutanddry/qa/functions/Customer.java
@@ -193,6 +193,9 @@ public class Customer {
         customersPage.clickOnDownloadOrderGuide();
     }
     public static void searchItemOnOrderGuide(String item) throws InterruptedException {
+        if (customersPage.isPreviousDraftOrderNoDisplayed()){
+            customersPage.clickPreviousDraftOrderNo();
+        }
         customersPage.typeToSearchOnOrderGuide(item);
     }
     public static void goToCreatePopup() throws InterruptedException {
