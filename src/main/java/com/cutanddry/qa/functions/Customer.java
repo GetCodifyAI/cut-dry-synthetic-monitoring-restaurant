cd256ce2cbfcacb500405e805e7e6aa82724a096
senuri-mr <senuri@cutanddry.com>
2024-11-12 18:00:23 +0530

    Modified

--- src/main/java/com/cutanddry/qa/functions/Customer.java
+++ src/main/java/com/cutanddry/qa/functions/Customer.java
@@ -193,9 +193,9 @@ public class Customer {
         customersPage.clickOnDownloadOrderGuide();
     }
     public static void searchItemOnOrderGuide(String item) throws InterruptedException {
-        if (customersPage.isPreviousDraftOrderNoDisplayed()){
-            customersPage.clickPreviousDraftOrderNo();
-        }
+//        if (customersPage.isPreviousDraftOrderNoDisplayed()){
+//            customersPage.clickPreviousDraftOrderNo();
+//        }
         customersPage.typeToSearchOnOrderGuide(item);
     }
     public static void goToCreatePopup() throws InterruptedException {
