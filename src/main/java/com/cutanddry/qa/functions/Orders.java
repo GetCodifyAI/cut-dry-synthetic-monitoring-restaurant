6dcc05370b50323a0b57c0908b0338273623c569
senuri-mr <senuri@cutanddry.com>
2024-11-19 14:49:28 +0530

    Modified

--- src/main/java/com/cutanddry/qa/functions/Orders.java
+++ src/main/java/com/cutanddry/qa/functions/Orders.java
@@ -2,13 +2,18 @@ package com.cutanddry.qa.functions;
 
 import com.cutanddry.qa.pages.OrdersPage;
 
+import static com.cutanddry.qa.functions.Customer.customersPage;
+
 public class Orders {
     static OrdersPage ordersPage = new OrdersPage();
 
     public static boolean isUserNavigatedToOrderGuide(){
         return ordersPage.isOrderGuideTextDisplayed();
     }
-    public static void navigateToOrderGuide(String name) {
+    public static void navigateToOrderGuide(String name) throws InterruptedException {
         ordersPage.clickOnPlaceOrder(name);
+        if (customersPage.isPreviousDraftOrderNoDisplayed()){
+            customersPage.clickPreviousDraftOrderNo();
+       }
     }
 }
