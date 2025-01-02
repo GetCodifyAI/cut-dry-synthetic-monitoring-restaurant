49de2e2e2ba7371045d06dd14b65e70fc3a8675a
UmeshIshan25 <umesh@cutanddry.com>
2025-01-02 11:25:50 +0530

    Changed the Restaurant name to Business

--- src/main/java/com/cutanddry/qa/pages/DashboardPage.java
+++ src/main/java/com/cutanddry/qa/pages/DashboardPage.java
@@ -36,7 +36,7 @@ public class DashboardPage extends LoginPage{
     By btn_trackNotifications = By.xpath("//div[@arrowprops]//a[text()='Notifications']");
     By btn_pay  =By.xpath("//a[@data-tip='View Invoices']");
     By btn_reports  =By.xpath("//a[@data-tip='Reports']");
-    By btn_restaurantSettings = By.xpath("//div[@arrowprops]//a[text()='Restaurant']");
+    By btn_restaurantSettings = By.xpath("//div[@arrowprops]//a[text()='Business']");
     By btn_profSettings = By.xpath("//div[@arrowprops]//a[text()='Profile']");
     By btn_accountSettings = By.xpath("//div[@arrowprops]//a[text()='Accounting']");
     By btn_locationSettings = By.xpath("//div[@arrowprops]//a[text()='Locations']");
