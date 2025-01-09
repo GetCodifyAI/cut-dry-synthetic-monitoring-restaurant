eebc2efeb2d17559777037609719cce1c07bd2a8
UmeshIshan25 <umesh@cutanddry.com>
2025-01-09 22:31:26 +0530

    Testing the new Webhook

--- src/main/java/com/cutanddry/qa/common/Constants.java
+++ src/main/java/com/cutanddry/qa/common/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
     public static String SEC_URL = secDomain();
     public static String NODE_EXPLORER = "https://app-uat.staging.cutanddry.com/admin";
     public static String BASE_URI = "https://supplier-uat.staging.cutanddry.com/GraphQLController";
-    public static String SLACK_WEBHOOK = "https://hooks.slack.com/services/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";
+    public static String SLACK_WEBHOOK = "https://hooks.slack.com/services/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";
     public static String CONFIG_SUPPLIER = "https://app-uat.staging.cutanddry.com/internaltools/configure-supplier";
     public static String PROD_LOGIN_AS = "https://internal.cutanddry.com/internaltools/loginas";
 
