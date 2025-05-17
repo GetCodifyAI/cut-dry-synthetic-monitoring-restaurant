98835aefa62aba1530b41a8faf88682cef236fe7
AhsanMubarak <ahsan@cutanddry.com>
2025-05-17 14:03:48 +0530

    Update support CTA link on Operator App.(https://getcodify.atlassian.net/browse/DDO-696)

--- src/main/java/com/cutanddry/qa/pages/SupportPage.java
+++ src/main/java/com/cutanddry/qa/pages/SupportPage.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.By;
 
 public class SupportPage extends TestBase {
 
-    By support_center = By.xpath("//div[@class='hf-header-support-center-name']");
+//    By support_center = By.xpath("//div[@class='hf-header-support-center-name']");
+By support_center = By.xpath("//h1[contains(text(),'How Can We Help You?')]");
 
     public boolean isSupportCenterHeaderDisplayed(){
         distributorUI.waitForVisibility(support_center);
